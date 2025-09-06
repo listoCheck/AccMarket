174eb40cba6bde46b12534aa3c72acc6b0a1d49f
artem <v.amuzinsky@yandex.ru>
2025-09-06 13:43:33 +0300

    Backend: first commit
    Signed-off-by: artem <v.amuzinsky@yandex.ru>

---
+++ src/main/kotlin/com/example/accmarket/AccMarketApplication.kt
@@ -0,0 +1,11 @@
+package com.example.accmarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccMarketApplication
+
+fun main(args: Array<String>) {
+    runApplication<AccMarketApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/accmarket/AccMarketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.accmarket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccMarketApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
